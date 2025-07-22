947ab92d389536c9e28b1494f5dad8fa8603a8b7
RailsonF <railsonfurtado16@gmail.com>
2025-07-21 21:19:38 -0300

    refatorando o código

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,33 @@ const footer = document.querySelector("footer")
 
 
 //Função que vai apenas calcular o valor
-function calculateConversion(rate, value) {
-    result.innerHTML = (inputValue * valorDollar).toFixed(2) + " R$"
+function calculateConversion(value, rate) {
+    return (value * rate).toFixed(2) + " R$"
 }
+
+//Função que formata moedas
+function formatedCoin(element, typeOfCoin) {
+    if (typeOfCoin === "USD") {
+        element.innerHTML =`US$ 1 = ${valorDollar}R$`
+        return element
+    }
+
+    else if (typeOfCoin === "EUR"){
+        element.innerHTML = `€ 1 = ${valorEuro}R$`
+        return element
+    }
+
+    else  if(typeOfCoin === "GBP") {
+        element.innerHTML = `£ 1 = ${valorLibra}R$`
+        return element
+    }
+}
+
+function typeOfDisplay(element, display = "block") {
+     return element.style.display = display
+}
+
+
 //Lógica do evento
 form.addEventListener('submit', (event) => {
     //Impedindo o comportamento padrão de enviar o forme recarregar a página
@@ -33,21 +57,23 @@ form.addEventListener('submit', (event) => {
         description.innerHTML = ""
         return //encerrando a função
     }
-    if (selectvalue == "USD") {
-        footer.style.display = "block"
-        calculateConversion(inputValue)
-        result.innerHTML = (inputValue * valorDollar).toFixed(2) + " R$"
-        description.innerHTML = `US$ 1 = ${valorDollar}R$`
-
+    switch (selectvalue) {
+        case "USD":
+            typeOfDisplay(footer)
+            result.innerHTML = calculateConversion(inputValue, valorDollar)
+            typeOfCoin(description,"USD")
+            break;
+        case "EUR":
+            typeOfDisplay(footer)
+            result.innerHTML = calculateConversion(inputValue, valorEuro)
+            typeOfCoin(description, "EUR")
+            break;
+        case "GBP":
+            typeOfDisplay(footer)
+            result.innerHTML = calculateConversion(inputValue, valorLibra)
+            typeOfCoin(description, "GBP")
+        default:
+            break;
     }
-    else if (selectvalue == "EUR") {
-        footer.style.display = "block"
-        result.innerHTML = (inputValue * valorEuro).toFixed(2) + " R$"
-        description.innerHTML = `€ 1 = ${valorEuro}R$`
-    }
-    else if (selectvalue == "GBP") {
-        footer.style.display = "block"
-        result.innerHTML = (inputValue * valorLibra).toFixed(2) + " R$"
-        description.innerHTML = `£ 1 = ${valorLibra}R$`
-    }
+    
 })
